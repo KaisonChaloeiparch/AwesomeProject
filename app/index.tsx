16a6e604b9b7fb0b63a563bf1690121e24ddf489
KaisonChaloeiparch <151432957+KaisonChaloeiparch@users.noreply.github.com>
2025-07-06 20:48:48 +0700

    today06072025

---
+++ app/index.tsx
@@ -0,0 +1,19 @@
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+        <Link href="/test-greeting" style={styles.link}>
+        <Text style={styles.linkText}>Test Greeting</Text>
+      </Link>
+    </View>
+    
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {    flex: 1,    justifyContent: "center",    alignItems: "center",  },
+  link: {    margin: 10,    padding: 10,    backgroundColor: "#2196F3",    borderRadius: 5,  },
+  linkText: {    color: "#fff",    fontSize: 16,    textAlign: "center",  },
+});
--- app/test-components.tsx
+++ app/test-components.tsx
@@ -1,68 +1,80 @@
 import { FontAwesome } from '@expo/vector-icons';
-import { Link } from 'expo-router';
 import React from 'react';
-import { Alert, Button, Image, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Alert, Button, Image, ScrollView, Text, TextInput, View } from 'react-native';
 
 
 export default function TestComponent() {
-    return (
-        <ScrollView style={{ margin: 20 }} >
+  return (
+    <ScrollView  style={{ margin : 20 }} >
 
-            <TextInput
-                keyboardType="email-address"
-                autoCapitalize="none"
-                autoCorrect={false}
-                maxLength={30}
-                multiline={false}
-                placeholder="Please enter your email"
-                returnKeyType="next"
-                onChangeText={(text) => { }}
-                defaultValue="Default text"
-            />
-            <Button
-                title="Press me"
-                onPress={() => Alert.alert("Simple Button pressed")}
-                color="#665544"
-            />
+        <TextInput
+  keyboardType="email-address"
+  autoCapitalize="none"
+  autoCorrect={false}
+  maxLength={30}
+  multiline={false}
+  placeholder="Please enter your email"
+  returnKeyType="next"
+  onChangeText={(text)=>{}}
+  defaultValue="Default text"
+/>
 
-            <Text> Hello World </Text>
+<Button
+  title="Press me now!!!!"
+  onPress={() => Alert.alert("Simple Button pressed")}
+  color="#665544"
+/>
 
-            <Image source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }} style={{ width: 50, height: 50 }} />
+<Text> Hello World Kaison Chaloeiprach </Text>
 
-            <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 50 }}>
-                <Image style={{ width: 100, height: 50 }} source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }} />
-                <Image style={{ width: 100, height: 100 }} source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }} />
-                <Image style={{ width: 100, height: 100, borderRadius: 100 / 2 }} source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }} />
-            </View>
+<Image source={{ uri : 'https://reactnative.dev/img/tiny_logo.png' }} style={{ width:100, height:100 }} />
 
-            <View style={{ flexDirection: 'row', marginTop: 20 }}>
-                <Image style={{ flex: 1, resizeMode: 'cover', aspectRatio: 3 / 2 }} source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }} />
-            </View>
+<View style={{ flexDirection: 'column' }}>
+    {/* Fixed Size */}
+    <View style={{ flexDirection: 'row' , justifyContent : 'space-between' , marginTop : 50}}>
+        <Image style={{ width: 100, height: 100 }} source={{ uri : 'https://reactnative.dev/img/tiny_logo.png' }} />
+        <Image style={{ width: 100, height: 100}} source={{ uri : 'https://reactnative.dev/img/tiny_logo.png' }} />
+        <Image style={{ width: 100, height: 100, borderRadius: 100/2 }} source={{ uri : 'https://reactnative.dev/img/tiny_logo.png' }} />
+    </View>    
+</View>
 
-            <View style={{ flexDirection: 'row', marginTop: 20 }}>
-                <Image style={{ flex: 1, resizeMode: 'cover', aspectRatio: 16 / 4 }} source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }} />
-            </View>
+<View style={{ flexDirection: 'column' }}>
+    
+    <View style={{ flexDirection: 'row' , marginTop : 20 }}>
+        <Image style={{ flex: 1, resizeMode: 'cover', aspectRatio: 3 / 2 }} source={{ uri : 'https://reactnative.dev/img/tiny_logo.png' }} />
+    </View>
 
-            <View style={{ flexDirection: 'row', marginTop: 20, backgroundColor: 'gray' }}>
-                <Image style={{ flex: 1, resizeMode: 'contain', aspectRatio: 16 / 4 }} source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }} />
-            </View>
+    <View style={{ flexDirection: 'row' , marginTop : 20 }}>
+        <Image style={{ flex: 1, resizeMode: 'cover', aspectRatio: 16 / 4 }} source={{ uri : 'https://reactnative.dev/img/tiny_logo.png' }} />
+    </View>
 
-            <FontAwesome name="heart"  size={32} color="orange" />
+    <View style={{ flexDirection: 'row' , marginTop : 20, backgroundColor : 'gray' }}>
+        <Image style={{ flex: 1, resizeMode: 'contain', aspectRatio: 16 / 4 }} source={{ uri : 'https://reactnative.dev/img/tiny_logo.png' }} />
+    </View>
+
+</View>
+
+<View style={{ height: 50, width: 50, borderRadius: 50/2, backgroundColor: 'red', justifyContent: 'center', alignItems: 'center' }}>
+    <Text style={{ fontSize: 30, color : "white" }}>หัวใจ</Text>
+</View>
+<FontAwesome name="heart"  size={50} color="orange" />
+import { <FontAwesome></FontAwesome> } from '@expo/vector-icons';
+
+<ScrollView>
+    <View style={{ flex: 1, flexDirection: 'column' }}>
+        ...               
+    </View>
+</ScrollView>
 
 
 
-            style={SceneStyleInterpolators.container}
 
-            <Link href="/test-components" style={styles.link}>
-                <Text style={styles.linkText}>Test Components</Text>
-            </Link>
 
-        </ScrollView>
-    );
+
+
+
+
+
+    </ScrollView>
+  );
 }
-
-const styles = StyleSheet.create({
-    container: { flex: 1, justifyContent: "center", alignItems: "center", },
-    link: { margin: 10, padding: 10, backgroundColor: "#2196F3", borderRadius: 5, },
-    linkText: { color: "#fff", fontSize: 16, textAlign: "center", },
-});
