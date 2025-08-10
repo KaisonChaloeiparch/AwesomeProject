fe92ce4bde0971a47b575212d1216a9da8174868
KaisonChaloeiparch <151432957+KaisonChaloeiparch@users.noreply.github.com>
2025-08-10 13:50:46 +0700

    timenow

--- app/index.tsx
+++ app/index.tsx
@@ -8,79 +8,84 @@ export default function Index() {
   return (
 
     <ScrollView>
-    <View style={styles.container}>
+      <View style={styles.container}>
 
-      <Link href="/todolist" style={styles.link}>
-        <Text style={styles.linkText}>Todo List</Text>
-      </Link>
-
-
-      <Link href="/your-health" style={styles.link}>
-        <Text style={styles.linkText}>Your Health</Text>
-      </Link>
+        <Link href="/todolist" style={styles.link}>
+          <Text style={styles.linkText}>Todo List</Text>
+        </Link>
 
 
+        <Link href="/your-health" style={styles.link}>
+          <Text style={styles.linkText}>Your Health</Text>
+        </Link>
 
-
-      <Link href="/home" style={styles.link}>
-        <Text style={styles.linkText}>Home</Text>
-      </Link>
+        <Link href="/chart" style={styles.link}>
+          <Text style={styles.linkText}>Chart</Text>
+        </Link>
 
 
 
 
-      <Link href="/flatlistexample" style={styles.link}>
-        <Text style={styles.linkText}>Flatlist Example</Text>
-      </Link>
 
-
-      <Link href="/health" style={styles.link}>
-        <Text style={styles.linkText}>Health</Text>
-      </Link>
-
-
-      <Link href="/travel" style={styles.link}>
-        <Text style={styles.linkText}>Travel</Text>
-      </Link>
-
-      <Link href="/resort" style={styles.link}>
-        <Text style={styles.linkText}>Resort</Text>
-      </Link>
-
-      <Link href="/review" style={styles.link}>
-        <Text style={styles.linkText}>Review</Text>
-      </Link>
-
-
-      <Link
-        href="/flexbox/ex12"
-        style={{
-          marginTop: 24,
-          padding: 12,
-          backgroundColor: "#007AFF",
-          borderRadius: 8,
-        }}
-      >
-
-        <Text style={{ color: "#fff", fontWeight: "bold" }}>เด้งไปหน้าที่ 2</Text>
-      </Link>
+        <Link href="/home" style={styles.link}>
+          <Text style={styles.linkText}>Home</Text>
+        </Link>
 
 
 
 
-      {/* <Link href="/test-components" style={styles.link}> */}
-      {/* <Text style={styles.linkText}>Test Components</Text> */}
-      {/* </Link> */}
+        <Link href="/flatlistexample" style={styles.link}>
+          <Text style={styles.linkText}>Flatlist Example</Text>
+        </Link>
 
 
-      <Text>Flexbox Menu</Text>
-      <FlexboxMenu />
+        <Link href="/health" style={styles.link}>
+          <Text style={styles.linkText}>Health</Text>
+        </Link>
 
 
-      <Link href="/test-greeting" style={styles.link}>
-        <Text style={styles.linkText}>Test Greeting</Text>
-      </Link>
-    </View>
+        <Link href="/travel" style={styles.link}>
+          <Text style={styles.linkText}>Travel</Text>
+        </Link>
+
+        <Link href="/resort" style={styles.link}>
+          <Text style={styles.linkText}>Resort</Text>
+        </Link>
+
+        <Link href="/review" style={styles.link}>
+          <Text style={styles.linkText}>Review</Text>
+        </Link>
+
+
+        <Link
+          href="/flexbox/ex12"
+          style={{
+            marginTop: 24,
+            padding: 12,
+            backgroundColor: "#007AFF",
+            borderRadius: 8,
+          }}
+        >
+
+          <Text style={{ color: "#fff", fontWeight: "bold" }}>เด้งไปหน้าที่ 2</Text>
+        </Link>
+
+
+
+
+        {/* <Link href="/test-components" style={styles.link}> */}
+        {/* <Text style={styles.linkText}>Test Components</Text> */}
+        {/* </Link> */}
+
+
+        <Text>Flexbox Menu</Text>
+        <FlexboxMenu />
+
+
+        <Link href="/test-greeting" style={styles.link}>
+          <Text style={styles.linkText}>Test Greeting</Text>
+        </Link>
+      </View>
 
     </ScrollView>
 
