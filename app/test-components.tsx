9746bc48f78abac6aeff966dd1586022229d13bb
KaisonChaloeiparch <151432957+KaisonChaloeiparch@users.noreply.github.com>
2025-08-11 20:21:33 +0700

    now

--- app/test-components.tsx
+++ app/test-components.tsx
@@ -4,77 +4,70 @@ import { Alert, Button, Image, ScrollView, Text, TextInput, View } from 'react-n
 
 
 export default function TestComponent() {
-  return (
-    <ScrollView  style={{ margin : 20 }} >
+    return (
+        <ScrollView style={{ margin: 20 }} >
 
-        <TextInput
-  keyboardType="email-address"
-  autoCapitalize="none"
-  autoCorrect={false}
-  maxLength={30}
-  multiline={false}
-  placeholder="Please enter your email"
-  returnKeyType="next"
-  onChangeText={(text)=>{}}
-  defaultValue="Default text"
-/>
+            <TextInput
+                keyboardType="email-address"
+                autoCapitalize="none"
+                autoCorrect={false}
+                maxLength={30}
+                multiline={false}
+                placeholder="Please enter your email"
+                returnKeyType="next"
+                onChangeText={(text) => { }}
+                defaultValue="Default text"
+            />
 
-<Button
-  title="Press me now!!!!"
-  onPress={() => Alert.alert("Simple Button pressed")}
-  color="#665544"
-/>
+            <Button
+                title="Press me now!!!!"
+                onPress={() => Alert.alert("Simple Button pressed")}
+                color="#665544"
+            />
 
-<Text> Hello World Kaison Chaloeiprach </Text>
+            <Text> Hello World Kaison Chaloeiprach </Text>
+            <Text> My name is Poppy love you. </Text>
+            <Text> My name is Poppy love me.</Text>
 
-<Image source={{ uri : 'https://reactnative.dev/img/tiny_logo.png' }} style={{ width:100, height:100 }} />
+            <Image source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }} style={{ width: 100, height: 100 }} />
 
-<View style={{ flexDirection: 'column' }}>
-    {/* Fixed Size */}
-    <View style={{ flexDirection: 'row' , justifyContent : 'space-between' , marginTop : 50}}>
-        <Image style={{ width: 100, height: 100 }} source={{ uri : 'https://reactnative.dev/img/tiny_logo.png' }} />
-        <Image style={{ width: 100, height: 100}} source={{ uri : 'https://reactnative.dev/img/tiny_logo.png' }} />
-        <Image style={{ width: 100, height: 100, borderRadius: 100/2 }} source={{ uri : 'https://reactnative.dev/img/tiny_logo.png' }} />
-    </View>    
-</View>
+            <View style={{ flexDirection: 'column' }}>
+                {/* Fixed Size */}
+                <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 50 }}>
+                    <Image style={{ width: 100, height: 100 }} source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }} />
+                    <Image style={{ width: 100, height: 100 }} source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }} />
+                    <Image style={{ width: 100, height: 100, borderRadius: 100 / 2 }} source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }} />
+                </View>
+            </View>
 
-<View style={{ flexDirection: 'column' }}>
-    
-    <View style={{ flexDirection: 'row' , marginTop : 20 }}>
-        <Image style={{ flex: 1, resizeMode: 'cover', aspectRatio: 3 / 2 }} source={{ uri : 'https://reactnative.dev/img/tiny_logo.png' }} />
-    </View>
+            <View style={{ flexDirection: 'column' }}>
 
-    <View style={{ flexDirection: 'row' , marginTop : 20 }}>
-        <Image style={{ flex: 1, resizeMode: 'cover', aspectRatio: 16 / 4 }} source={{ uri : 'https://reactnative.dev/img/tiny_logo.png' }} />
-    </View>
+                <View style={{ flexDirection: 'row', marginTop: 20 }}>
+                    <Image style={{ flex: 1, resizeMode: 'cover', aspectRatio: 3 / 2 }} source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }} />
+                </View>
 
-    <View style={{ flexDirection: 'row' , marginTop : 20, backgroundColor : 'gray' }}>
-        <Image style={{ flex: 1, resizeMode: 'contain', aspectRatio: 16 / 4 }} source={{ uri : 'https://reactnative.dev/img/tiny_logo.png' }} />
-    </View>
+                <View style={{ flexDirection: 'row', marginTop: 20 }}>
+                    <Image style={{ flex: 1, resizeMode: 'cover', aspectRatio: 16 / 4 }} source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }} />
+                </View>
 
-</View>
+                <View style={{ flexDirection: 'row', marginTop: 20, backgroundColor: 'gray' }}>
+                    <Image style={{ flex: 1, resizeMode: 'contain', aspectRatio: 16 / 4 }} source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }} />
+                </View>
 
-<View style={{ height: 50, width: 50, borderRadius: 50/2, backgroundColor: 'red', justifyContent: 'center', alignItems: 'center' }}>
-    <Text style={{ fontSize: 30, color : "white" }}>หัวใจ</Text>
-</View>
-<FontAwesome name="heart"  size={50} color="orange" />
-import { <FontAwesome></FontAwesome> } from '@expo/vector-icons';
+            </View>
 
-<ScrollView>
-    <View style={{ flex: 1, flexDirection: 'column' }}>
-        ...               
-    </View>
-</ScrollView>
+            <View style={{ height: 50, width: 50, borderRadius: 50 / 2, backgroundColor: 'red', justifyContent: 'center', alignItems: 'center' }}>
+                <Text style={{ fontSize: 20, color: "white" }}>หัวใจ</Text>
+            </View>
+            <FontAwesome name="heart" size={50} color="orange" />
+            import {<FontAwesome></FontAwesome>} from '@expo/vector-icons';
 
+            <ScrollView>
+                <View style={{ flex: 1, flexDirection: 'column' }}>
+                    ...
+                </View>
+            </ScrollView>
 
-
-
-
-
-
-
-
-
-    </ScrollView>
-  );
+        </ScrollView>
+    );
 }
